13f401a31596e42c7c18a278de91c36de3703c74
PatrykHensa <patrykhensa@gmail.com>
2023-12-08 23:52:24 +0100

    fix forecasts sizing

--- script.js
+++ script.js
@@ -15,20 +15,20 @@ let zmienna = "Dynamiczny tekst";
 
 
 
-for (let i = 0; i < 3; i++){
-    let html = `<div class="col border border-primary " >
+for (let i = 0; i < 23; i++){
+    let html = `<div class="col border border-primary" >
 <div class="row">
     <div class="col col-12">${i+1}:00</div>
     <div class="col col-12">40%</div>
-    <div class="col col-12"><img src="Icons/clouds_sun_sunny_icon.svg" alt="" class="w-100"></div>
+    <div class="col col-12"><img src="Icons/clouds_sun_sunny_icon.svg" alt="" class="w-75"></div>
     <div class="col col-12">-2deg</div>
 </div>`;
 hoursContainer.innerHTML += html; 
 }
 
-for (let i = 0; i < 5; i++){
-    let html = `<button type="button" class="btn btn-outline-primary text-white p-0 ">
-    <div class="row border d-flex align-items-center d-flex flex-column flex-lg-row mx-0">
+for (let i = 0; i < 6; i++){
+    let html = `<button type="button" class="btn btn-outline-primary text-white p-0  h-100">
+    <div class="row border d-flex align-items-center flex-column flex-lg-row mx-0 h-100">
         <div class="col-3 d-flex justify-content-center">
             <p class="my-1">Mon</p>
             
