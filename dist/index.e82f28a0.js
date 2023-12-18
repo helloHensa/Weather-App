e8a3491bfffabe61311d98d63ec2522978beec7e
PatrykHensa <patrykhensa@gmail.com>
2023-12-18 18:20:23 +0100

    commit

--- dist/index.e82f28a0.js
+++ dist/index.e82f28a0.js
@@ -592,7 +592,7 @@ for(let i = 0; i < 23; i++){
 <div class="row">
     <div class="col col-12">${i + 1}:00</div>
     <div class="col col-12">40%</div>
-    <div class="col col-12"><img src="~Icons/clouds_sun_sunny_icon.svg" alt="" class="w-75"></div>
+    <div class="col col-12"><img src="dist/clouds_sun_sunny_icon.svg" alt="" class="w-75"></div>
     <div class="col col-12">-2deg</div>
 </div>`;
     hoursContainer.innerHTML += html;
