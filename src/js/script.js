6bedd523072517ae6214ec1877fcb657634fb4c9
PatrykHensa <patrykhensa@gmail.com>
2024-02-23 21:16:48 +0100

    fix search auto complete

--- src/js/script.js
+++ src/js/script.js
@@ -354,18 +354,66 @@ async function drawWeather(city) {
   uvIndexDesc.style.color = uvColor;
 };
 
+// search autocomplete
 const inputField = document.getElementById('search-input');
+const suggestionsContainer = document.getElementById('suggestions');
+
+inputField.addEventListener('keydown', function(event) {
+  const suggestions = suggestionsContainer.querySelectorAll('.dropdown-item');
+  const selectedIndex = Array.from(suggestions).findIndex(item => item.classList.contains('selected'));
+
+  if (event.key === 'ArrowUp' && selectedIndex > 0) {
+      event.preventDefault(); // Zapobiegnij domyślnemu działaniu, gdy kursor jest na początku listy
+      suggestions[selectedIndex].classList.remove('selected');
+      suggestions[selectedIndex - 1].classList.add('selected');
+  } else if (event.key === 'ArrowDown' && selectedIndex < suggestions.length - 1) {
+      event.preventDefault(); // Zapobiegnij domyślnemu działaniu, gdy kursor jest na końcu listy
+      if (selectedIndex !== -1) {
+          suggestions[selectedIndex].classList.remove('selected');
+      }
+      suggestions[selectedIndex + 1].classList.add('selected');
+  } else if (event.key === 'Enter') {
+      const selectedSuggestion = suggestionsContainer.querySelector('.selected');
+      if (selectedSuggestion) {
+          inputField.value = selectedSuggestion.textContent;
+          suggestionsContainer.style.display = 'none';
+          znazwy(inputField.value);
+      }
+  }
+});
 
 inputField.addEventListener('input', function() {
-    const inputValue = inputField.value;
-    fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${inputValue}&types=(cities)&key=${gApi}`)
-        .then(response => response.json())
-        .then(data => {
-            // Obsługa odpowiedzi z API - wyświetlenie sugerowanych miejsc w polu wyszukiwania
-        })
-        .catch(error => {
-            console.error('Błąd podczas pobierania sugestii miejsc:', error);
-        });
+  const inputValue = inputField.value;
+
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${inputValue}&key=${gApi}`)
+    .then(response => response.json())
+    .then(data => {
+        suggestionsContainer.innerHTML = '';
+        const suggestions = data.results;
+        if (suggestions.length > 0) { // Sprawdzenie, czy są jakiekolwiek sugestie
+            suggestions.forEach((suggestion, index) => {
+                console.log(suggestion.formatted_address)
+                const suggestionItem = document.createElement('li');
+                suggestionItem.classList.add('dropdown-item');
+                suggestionItem.textContent = suggestion.formatted_address;
+                suggestionItem.addEventListener('click', function() {
+                    inputField.value = suggestion.formatted_address;
+                    suggestionsContainer.style.display = 'none';
+                });
+                suggestionsContainer.appendChild(suggestionItem);
+
+                if (index === 0) {
+                    suggestionItem.classList.add('selected');
+                }
+            });
+            suggestionsContainer.style.display = 'block'; // Ustawienie display block, gdy są sugestie
+        } else {
+            suggestionsContainer.style.display = 'none'; // Ukrycie kontenera, gdy nie ma sugestii
+        }
+    })
+    .catch(error => {
+        console.error('Błąd podczas pobierania sugestii miejsc:', error);
+    });
 });
 
 //drawWeather('polska');
@@ -373,6 +421,6 @@ function zlokalizacji(){
   text = true;
   drawWeather('50.1437776, 18.7756856');
 }
-function znazwy(){
-  drawWeather('orzesze');
+function znazwy(value){
+  drawWeather(value);
 }
