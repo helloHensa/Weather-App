ae6ded8c4d18ec42a5617f6dcc9c15bafc95186e
PatrykHensa <patrykhensa@gmail.com>
2024-03-02 14:00:44 +0100

    Fix chart position and delete gh-pages

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ import Chart from 'chart.js/auto';
 import { format, fromUnixTime, setDate } from 'date-fns';
 import compromise from 'compromise';
 import apiKeys from './apiKey';
+import { mapWeatherIcon } from './mapWeatherIcon';
 
 const apiKey = apiKeys.openWeather;
 const gApi = apiKeys.google;
@@ -48,31 +49,6 @@ const dateFormatter = new Intl.DateTimeFormat(userLanguage, { weekday: 'long' })
 const dateFormatterShort = new Intl.DateTimeFormat(userLanguage, { weekday: 'short' });
 
 
-function mapWeatherIcon(iconCode) {
-  const iconMapping = {
-    '01d': 'bi-sun',         // Bezchmurne niebo (dzień)
-    '01n': 'bi-moon',        // Bezchmurne niebo (noc)
-    '02d': 'bi-cloud-sun',   // Niewielkie zachmurzenie (dzień)
-    '02n': 'bi-cloud-moon',  // Niewielkie zachmurzenie (noc)
-    '03d': 'bi-cloud',       // Częściowe zachmurzenie (dzień)
-    '03n': 'bi-cloud',       // Częściowe zachmurzenie (noc)
-    '04d': 'bi-cloudy',      // Zachmurzenie (dzień)
-    '04n': 'bi-cloudy',      // Zachmurzenie (noc)
-    '09d': 'bi-cloud-drizzle', // Deszcz (dzień)
-    '09n': 'bi-cloud-drizzle', // Deszcz (noc)
-    '10d': 'bi-cloud-rain',  // Deszcz (intensywny, dzień)
-    '10n': 'bi-cloud-rain',  // Deszcz (intensywny, noc)
-    '11d': 'bi-cloud-lightning', // Burza (dzień)
-    '11n': 'bi-cloud-lightning', // Burza (noc)
-    '13d': 'bi-snow',        // Opady śniegu (dzień)
-    '13n': 'bi-snow',        // Opady śniegu (noc)
-    '50d': 'bi-fog',         // Mgła (dzień)
-    '50n': 'bi-fog',         // Mgła (noc)
-  };
-  const iconClass = iconMapping[iconCode];
-
-  return iconClass || 'bi-question';
-}
 checkStoredLocation();
 
 //search window behavior
@@ -542,8 +518,6 @@ deviceLocation.addEventListener('click', function(){
 searchButton.addEventListener('click', function() {
   znazwy(inputField.value);
 });
-
-
 
 
 function znazwy(value){
